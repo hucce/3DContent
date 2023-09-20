6866a103c6fcf1d56c86d21c51f6b57e111020e2
허민구 <huccce@gmail.com>
2023-09-20 21:00:10 +0900

    좀비, 플레이어
    - 좀비 플레이어 피격, 죽음 구현
    - 플레이어 상태머신 구현
    - 플레이어 상태머신에 따라 이동, 공격 수정

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,57 +4,134 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Animator animator = null;
+    public int HP = 100;
+
     // 변수
     public float moveSpeed = 0.2f;
+
+    State state = State.Idle;
+
+    public float attackSecond = 1;
+
+
+    public enum State
+    {
+        Idle, Move, Attack, Hit, Death
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float ver = Input.GetAxis("Vertical");
-        float hor = Input.GetAxis("Horizontal");
 
-        // 조건문 ver가 0이 아닌 경우
-        // ex) 같다 ==, 같지 않은 경우 !=, 초과 <, 이상(같거나 많다) <=
-        // || > 또는 or, and && > 그리고
-        if (ver != 0 || hor != 0)
+        Move();
+        Attack();
+    }
+
+    private void Move()
+    {
+        if (state == State.Idle || state == State.Move)
         {
-            GetComponent<Animator>().SetBool("isMove", true);
+            float ver = Input.GetAxis("Vertical");
+            float hor = Input.GetAxis("Horizontal");
 
-            Vector3 moveVector = new Vector3();
-            moveVector.x = hor * moveSpeed * Time.deltaTime;
-            moveVector.z = ver * moveSpeed * Time.deltaTime;
-
-            transform.position = transform.position + moveVector;
-
-            Vector3 checkVector = new Vector3();
-            if(moveVector != checkVector)
+            // 조건문 ver가 0이 아닌 경우
+            // ex) 같다 ==, 같지 않은 경우 !=, 초과 <, 이상(같거나 많다) <=
+            // || > 또는 or, and && > 그리고
+            if (ver != 0 || hor != 0)
             {
-                transform.forward = moveVector;
+                // 이동이 되면 이동 상태로 변경
+                state = State.Move;
+                animator.SetBool("isMove", true);
+
+                Vector3 moveVector = new Vector3();
+                moveVector.x = hor * moveSpeed * Time.deltaTime;
+                moveVector.z = ver * moveSpeed * Time.deltaTime;
+
+                transform.position = transform.position + moveVector;
+
+                Vector3 checkVector = new Vector3();
+                if (moveVector != checkVector)
+                {
+                    transform.forward = moveVector;
+                }
             }
+            else
+            {
+                // 이동 상태가 끝나면 다시 대기상태로 변경
+                state = State.Idle;
+                animator.SetBool("isMove", false);
+            }
+        }
+    }
+
+    void Attack()
+    {
+        StartCoroutine(CoAttack());
+    }
+
+    IEnumerator CoAttack()
+    {
+        if(state == State.Idle)
+        {
+            bool att = Input.GetButtonDown("Fire1");
+
+            if (att == true)
+            {
+                // 공격 버튼을 누르면 어택상태로 변경
+                state = State.Attack;
+                animator.SetBool("isMove", false);
+                animator.SetTrigger("isAttack");
+
+                // 일정 시간동안 대기한다.
+                yield return new WaitForSeconds(attackSecond);
+                // yield return () null > 1프레임 대기한다.
+                //new WaitForSeconds 특정 초를 대기한다.
+
+                state = State.Idle;
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "ZombieHand")
+        {
+            Hit();
+        }
+    }
+
+    private void Hit()
+    {
+        // 맞았으면 피격 상태로 변경
+        state = State.Hit;
+
+        // HP가 -가 된다. hp = hp-상대방의 공격력
+        HP = HP - 10;
+
+        // 만약 HP가 0이하라면 죽어야한다.
+        if (HP <= 0)
+        {
+            Death();
         }
         else
         {
-            GetComponent<Animator>().SetBool("isMove", false);
+            // 애니메이션 출력(피격)
+            animator.SetTrigger("isHit");
         }
+    }
 
+    private void Death()
+    {
+        //죽었으면 죽음 상태로 변경
+        state = State.Death;
 
-
-
-
-
-
-        bool att = Input.GetButtonDown("Fire1");
-
-        if (att == true)
-        {
-            GetComponent<Animator>().SetTrigger("isAttack");
-        }
-
+        animator.SetBool("isDeath", true);
     }
 }
--- Assets/Scripts/Zombie.cs
+++ Assets/Scripts/Zombie.cs
@@ -8,6 +8,8 @@ public class Zombie : MonoBehaviour
     Animator animator = null;
     public GameObject playerObject = null;
     public float checkDistance = 5;
+    public float attackDistance = 2;
+    public int HP = 100;
 
     // Start is called before the first frame update
     void Start()
@@ -24,22 +26,68 @@ public class Zombie : MonoBehaviour
     // 대기, 추격(이동), 공격, 피격, 죽음
     void Idle()
     {
-        animator.SetBool("isMove", false);
-
         Vector3 playerVector = playerObject.transform.position;
         Vector3 zombieVector = transform.position;
         float playerZombieDistance = Vector3.Distance(zombieVector, playerVector);
 
         // 좀비와 플레이어 사이의 거리가 일정 수치 이하일 때(가까울 때)
-        if(playerZombieDistance < checkDistance)
+        if(playerZombieDistance < attackDistance)
         {
+            // 공격범위 안이라면 공격
+            Attack();
+        }
+        else if (playerZombieDistance < checkDistance)
+        {
+            // 공격범위는 밖이고 이동 범위는 안인 경우
             // 추격한다.
             Move();
+        }
+        else
+        {
+            // 다시 거리가 멀어졌다면 혹은 원래부터 거리가 멀었다면 move애니메이션을 false로 만든다.
+            animator.SetBool("isMove", false);
         }
     }
 
     private void Move()
     {
+        animator.SetBool("isMove", true);
         GetComponent<NavMeshAgent>().destination = playerObject.transform.position;
+    }
+
+    private void Attack()
+    {
+        animator.SetTrigger("isAttack");
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "PlayerSword")
+        {
+            Hit();
+        }
+    }
+
+    private void Hit()
+    {
+        // HP가 -가 된다. hp = hp-상대방의 공격력
+        HP = HP - 10;
+
+        // 만약 HP가 0이하라면 죽어야한다.
+        if (HP <= 0)
+        {
+            Death();
+        }
+        else
+        {
+            // 애니메이션 출력(피격)
+            animator.SetTrigger("isHit");
+        }
+
+    }
+
+    private void Death()
+    {
+        animator.SetBool("isDeath", true);
     }
 }
